3a9eeac24104674a84235d07ccb2249a136b632c
ChrisChou <1050434689@qq.com>
2018-06-14 14:07:03 +0800

    创建用户说明

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 **暂时没有写readme.md**
+
+`创建管理员用户`
+```
+python3 manage.py createsuperuser
+```
--- my_admin/models.py
+++ my_admin/models.py
@@ -2,5 +2,3 @@ from my_admin import item_models
 from my_admin import member_models
 
 
-def build_form_data(fields, data):
-    return { i:data.get(i) for i in fields }
