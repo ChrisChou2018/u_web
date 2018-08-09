6a74fdb1b943c28237c4b673694464d061acddff
ChrisChou <1050434689@qq.com>
2018-08-09 11:22:08 +0800

    修复一个bug

--- ubskin_web_django/ad/views.py
+++ ubskin_web_django/ad/views.py
@@ -16,50 +16,51 @@ def my_render(request, templater_path, **kwargs):
     return render(request, templater_path, dict(**kwargs))
 
 def campaigns_manage(request):
-    search_dict = {
-        'search_value': 'campaign_name__icontains',
-        'location': 'location',
-    }
-    search_value = dict()
-    current_page = request.GET.get('page', 1)
-    filter_args = '&'
-    for i in search_dict:
-        value = request.GET.get(i)
-        if value is not None:
-            search_value[search_dict[i]] = value
-            filter_args += "{}={}".format(i, value)
-    else:
-        if len(filter_args) == 1:
-            filter_args = None
-    if search_value:
-        data_list = ad_models.get_data_list(
-            ad_models.Campaigns,
-            current_page,
-            search_value=search_value
+    if request.method == "GET":
+        search_dict = {
+            'search_value': 'campaign_name__icontains',
+            'location': 'location',
+        }
+        search_value = dict()
+        current_page = request.GET.get('page', 1)
+        filter_args = ""
+        for i in search_dict:
+            value = request.GET.get(i)
+            if value is not None:
+                search_value[search_dict[i]] = value
+                filter_args  += "&{}={}".format(i, value)
+        else:
+            if not filter_args:
+                filter_args = None
+        if search_value:
+            data_list = ad_models.get_data_list(
+                ad_models.Campaigns,
+                current_page,
+                search_value=search_value
+            )
+            data_count = ad_models.get_data_count(
+                ad_models.Campaigns,
+                search_value,
+            )
+        else:
+            data_list = ad_models.get_data_list(
+                ad_models.Campaigns,
+                current_page,
+            )
+            data_count = ad_models.get_data_count(
+                ad_models.Campaigns,
+            )
+        location_list = ad_models.Campaigns.get_all_location_list()
+        return my_render(
+            request,
+            'ad/a_campaigns_manage.html',
+            current_page = current_page,
+            form_data = request.GET,
+            filter_args = filter_args,
+            data_list = data_list,
+            data_count = data_count,
+            location_list = location_list,
         )
-        data_count = ad_models.get_data_count(
-            ad_models.Campaigns,
-            search_value,
-        )
-    else:
-        data_list = ad_models.get_data_list(
-            ad_models.Campaigns,
-            current_page,
-        )
-        data_count = ad_models.get_data_count(
-            ad_models.Campaigns,
-        )
-    location_list = ad_models.Campaigns.get_all_location_list()
-    return my_render(
-        request,
-        'ad/a_campaigns_manage.html',
-        current_page = current_page,
-        form_data = request.GET,
-        filter_args = filter_args,
-        data_list = data_list,
-        data_count = data_count,
-        location_list = location_list,
-    )
 
 class AddCampaignForm(forms.ModelForm):
     datetime = forms.CharField(error_messages={'required': '至少这个不可以为空'})
