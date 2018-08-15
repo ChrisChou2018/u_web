b011f0a4c41c1a9ca270fc5d8da9382120a3acd3
ChrisChou <1050434689@qq.com>
2018-08-15 14:39:41 +0800

    小修改

--- ubskin_web_django/item/urls.py
+++ ubskin_web_django/item/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path('myadmin/item_comment_manage/', views.item_comment_manage, name='item_comment_manage'),
     path('myadmin/edit_item_comment/', views.edit_item_comment, name='edit_item_comment'),
     path('myadmin/comment_image_manage/', views.item_comment_image_manage, name='comment_image_manage'),
-    path('create_brand/', views.create_brand),
+    # path('create_brand/', views.create_brand),
 ]
 
 urlpatterns += [
